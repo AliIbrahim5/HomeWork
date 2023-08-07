9ffab22d943f5d8bfcc91de3e93cf8dd722d8f08
AliIbrahim5 <ali.alyahya514@gmail.com>
2023-08-07 18:57:03 +0300

    Day1

---
+++ day1/day_1/index.js
@@ -0,0 +1,47 @@
+// let number = prompt("أدخل رقمًا:");
+// if (number == 0) {
+//   console.log("الرقم صفر");
+// } else {
+//   console.log("الرقم ليس صفرًا");
+// }
+
+// let age = 25;
+// let hasLicense = true;
+
+// if (age >= 18 && hasLicense) {
+//   console.log("يمكنك قيادة السيارة.");
+// } else {
+//   console.log("لا يمكنك قيادة السيارة.");
+// }
+
+// let isWeekend = true;
+// let isHoliday = false;
+
+// if (isWeekend || isHoliday) {
+//   console.log("هذا يوم عطلة!");
+// } else {
+//   console.log("هذا يوم عادي.");
+// }
+
+// let isLoggedIn = false;
+
+// if (!isLoggedIn) {
+//   console.log("يرجى تسجيل الدخول للمتابعة.");
+// } else {
+//   console.log("مرحبًا بعودتك!");
+// }
+
+// {
+//   let x = 10;
+//   console.log(x);
+// };
+
+// console.log(x);
+
+// {
+//   const y = 5;
+//   console.log(y);
+// }
+
+// console.log(y);
+
